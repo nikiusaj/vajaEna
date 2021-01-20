fe340438a2165dd99842cec5861073d6bbf2db10
Niki Ušaj <nikiusaj6@gmail.com>
2021-01-20 19:03:20 +0100

    First web app

---
+++ MVCFilmi/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFilmi.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Pozdravljen(string name="Niki",int num=1) {
+            ViewBag.Message = "Pozdravljen," + name;
+            ViewBag.Num = num;
+            return View();
+        }
+
+        public string Poz() {
+            return "Poz Svjt";
+        }
+    }
+}
---
+++ MVCFilmi/Models/Filmi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCFilmi.Models
+{
+    public class Filmi
+    {
+        public int Id{ get; set; }
+        public int Naslov { get; set; }
+        public int Izdanm { get; set; }
+        public int Tip { get; set; }
+        public int Cena { get; set; }
+    }
+}
